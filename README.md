8d98d68b7eb6432d4b8f5f68ec9f0e870a772f3c
JoaoVelasques <velasquesjmiguel@gmail.com>
2024-06-13 14:47:47 -0300

    Finish functions for playerChoice and computerChoice

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # odin-rock-paper-scissors
+
+A game of rock paper scissors being played on the console using javascript
+
+pseudocode:
+Make the computer pick between rock paper or scissors
+Make the player pick between rock paper and scissors
+Compare the two picks
+Come up with the round winner
+Play up to 5 rounds
+Declare the winner of the game
---
+++ javascript.js
@@ -0,0 +1,48 @@
+// Make a function in which the computer chooses between rock paper and scissors with math.random (1-3) and assigning rock, paper or scissors to each number//
+let computerPick = Math.floor(Math.random() * 3)
+console.log(computerPick)
+function computerChoice() {
+    if (computerPick === 0 ) {
+        return computerChoice = "rock"
+    }   else if (computerPick === 1) {
+        return computerChoice = "paper"
+    }   else {
+        return computerChoice = "scissors"
+    }   
+}
+console.log(computerChoice())
+
+// Make a function in which the player picks between rock paper and scissors //
+
+let playerPick = prompt("Please choose between Rock, Paper or Scissors")
+
+function playerChoice() {
+    if (playerPick.toLowerCase() === "rock") {
+        return playerChoice = "rock"
+    }   else if (playerPick.toLowerCase() === "paper") {
+        return playerChoice = "paper"
+    }   else if (playerPick.toLowerCase() === "scissors") {
+        return playerChoice = "scissors"
+    }   else {
+        return prompt("You must have made a typo! Please choose between Rock, Paper or Scissors")
+    }
+}
+
+console.log(playerChoice())
+
+
+// Compare the two picks //
+
+function compare(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        return console.log("You draw, play again!")
+    }
+    else if (playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "paper" && computerChoice === "rock" || playerChoice === "scissors" && computerChoice === "paper") {
+        return console.log("You win the round")
+    }
+    else {
+        return console.log("You loose the round")
+    }
+}
+
+console.log(compare(computerChoice, playerChoice))
