3e30ee6af526d01c19bd76d0f90fac08fd5d5ff3
JoaoVelasques <velasquesjmiguel@gmail.com>
2024-06-14 13:47:06 -0300

    Added semicolons in the script

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@
 
 
 function getComputerChoice() {
-let computerPick = Math.floor(Math.random() * 3)
+let computerPick = Math.floor(Math.random() * 3);
     if (computerPick === 0 ) {
-        return computerChoice = "rock"
+        return computerChoice = "rock";
 }   else if (computerPick === 1) {
-    return computerChoice = "paper"
+    return computerChoice = "paper";
     }   else {
-        return computerChoice = "scissors"
+        return computerChoice = "scissors";
 }   
 }
 //calls the getComputerChoice function.
@@ -17,15 +17,15 @@ getComputerChoice()
 
 //Prompts a player for a choice, converts it to lower case and if he makes a typo, prompts the player again.
 function getPlayerChoice() {
-    let playerPick = prompt("Please choose between Rock, Paper or Scissors")
+    let playerPick = prompt("Please choose between Rock, Paper or Scissors");
     if (playerPick.toLowerCase() === "rock") {
-        return playerChoice = "rock"
+        return playerChoice = "rock";
 }   else if (playerPick.toLowerCase() === "paper") {
-        return playerChoice = "paper"
+        return playerChoice = "paper";
     }   else if (playerPick.toLowerCase() === "scissors") {
-        return playerChoice = "scissors"
+        return playerChoice = "scissors";
     }   else {
-        return getPlayerChoice()
+        return getPlayerChoice();
     }
 }
 //calls the getPlayerChoice function.
@@ -43,67 +43,67 @@ let computerScore = 0;
 function round(computerChoice, playerChoice) {
 
     if (computerChoice === playerChoice) {
-        return console.log("You draw, play again!")
+        return console.log("You draw, play again!");
     }
     else if (playerChoice === "rock" && computerChoice === "scissors") {
-        playerScore += 1
-        console.log("You win this round! Rock beats scissors. Your score is " + playerScore + ". The computer score is " + computerScore)
-        return
+        playerScore += 1;
+        console.log("You win this round! Rock beats scissors. Your score is " + playerScore + ". The computer score is " + computerScore);
+        return;
     }
     else if (playerChoice === "paper" && computerChoice === "rock"){
-        playerScore += 1
-        console.log("You win this round! Paper beats rock. Your score is " + playerScore + ". The computer score is " + computerScore)
-        return
+        playerScore += 1;
+        console.log("You win this round! Paper beats rock. Your score is " + playerScore + ". The computer score is " + computerScore);
+        return;
     }
     else if (playerChoice === "scissors" && computerChoice === "rock"){
-        playerScore += 1
-        console.log("You win this round! Scissors beats rock. Your score is " + playerScore + ". The computer score is " + computerScore)
-        return
+        playerScore += 1;
+        console.log("You win this round! Scissors beats rock. Your score is " + playerScore + ". The computer score is " + computerScore);
+        return;
     }
     else if (playerChoice === "rock" && computerChoice === "paper"){
-    computerScore += 1
-    console.log("You've lost the round... Paper beats rock. Your score is " + playerScore + ". The computer score is " + computerScore)
-    return 
+    computerScore += 1;
+    console.log("You've lost the round... Paper beats rock. Your score is " + playerScore + ". The computer score is " + computerScore);
+    return;
     }
     else if (playerChoice === "scissors" && computerChoice === "rock"){
-        computerScore += 1
-        console.log("You've lost the round... Rock beats scissors. Your score is " + playerScore + ". The computer score is " + computerScore)
-        return 
+        computerScore += 1;
+        console.log("You've lost the round... Rock beats scissors. Your score is " + playerScore + ". The computer score is " + computerScore);
+        return;
     }
     else if (playerChoice === "paper" && computerChoice === "scissors"){
-        computerScore += 1
-        console.log("You've lost the round... Scissors beats paper. Your score is " + playerScore + ". The computer score is " + computerScore)
-        return 
+        computerScore += 1;
+        console.log("You've lost the round... Scissors beats paper. Your score is " + playerScore + ". The computer score is " + computerScore);
+        return;
     }
 }
 //calls the round function.
-round(computerChoice, playerChoice)
+round(computerChoice, playerChoice);
 //plays 4 more rounds of rock-paper-scissors.
-getComputerChoice()
-getPlayerChoice()
-round(computerChoice, playerChoice)
-getComputerChoice()
-getPlayerChoice()
-round(computerChoice, playerChoice)
-getComputerChoice()
-getPlayerChoice()
-round(computerChoice, playerChoice)
-getComputerChoice()
-getPlayerChoice()
-round(computerChoice, playerChoice)
+getComputerChoice();
+getPlayerChoice();
+round(computerChoice, playerChoice);
+getComputerChoice();
+getPlayerChoice();
+round(computerChoice, playerChoice);
+getComputerChoice();
+getPlayerChoice();
+round(computerChoice, playerChoice);
+getComputerChoice();
+getPlayerChoice();
+round(computerChoice, playerChoice);
 
 
 //calls the winner of the game and point results.
 if (playerScore > computerScore) {
-    console.log("You've won the game! " + playerScore + "-" + computerScore)
+    console.log("You've won the game! " + playerScore + "-" + computerScore);
     }
     else if (playerScore < computerScore) {
-        console.log("You've lost! " + playerScore + "-" + computerScore)
+        console.log("You've lost! " + playerScore + "-" + computerScore);
     }
     else {
-        console.log("You've drawn " + playerScore + "-" + computerScore)
+        console.log("You've drawn " + playerScore + "-" + computerScore);
     }
 }
 
 //calls the game function.
-game()
+game();
